96377a08947caa16e0969b8256bd6bb5fd0f402a
Joshua Basche <joshbasche@live.com>
2021-09-11 12:12:46 -0700

    add distance in AR as floating text attached to last point

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   
   @IBOutlet var sceneView: ARSCNView!
   
+  var dotNodes = [SCNNode]()
+  var textNode = SCNNode()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -42,6 +45,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if dotNodes.count >= 2 {
+      for dot in dotNodes {
+        dot.removeFromParentNode()
+      }
+      dotNodes = [SCNNode]()
+      
+    }
+    
     
     if let touchLocation = touches.first?.location(in: sceneView) {
       if let query = sceneView.raycastQuery(from: touchLocation, allowing: .estimatedPlane, alignment: .any) {
@@ -70,6 +81,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
     
     sceneView.scene.rootNode.addChildNode(dotNode)
+    
+    dotNodes.append(dotNode)
+    
+    if dotNodes.count >= 2 {
+      calculate()
+    }
+  }
+  
+  func calculate() {
+    let start = dotNodes[0]
+    let end = dotNodes[1]
+    
+    
+    let a = end.position.x - start.position.x
+    let b = end.position.y - start.position.y
+    let c = end.position.z - start.position.z
+    
+    // square root built in function
+    let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+//    distance = √ ((x2 - x1)2 + (y2 - y1)2 + (z2 - z1)2)
+//    let centimeters = distance * 100
+//    let roundedCent = round(centimeters)
+//    let feet = roundedCent / 30.48
+    // Print Absolute Distance no negative
+    updateText(text: "\(abs(distance)) Ft", atPosition: end.position)
+  }
+  
+  func updateText(text: String, atPosition position: SCNVector3) {
+    
+    textNode.removeFromParentNode()
+    
+    let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+    
+    textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+    
+    textNode = SCNNode(geometry: textGeometry)
+    textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+    textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+    
+    sceneView.scene.rootNode.addChildNode(textNode)
+    
   }
   
   
